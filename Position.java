e937624a421feb4d61411dc700e3c570e0720cfe
Alex Oliva <98245982+alexo46@users.noreply.github.com>
2022-06-08 11:05:26 -0500

    Update Position.java

--- Position.java
+++ Position.java
@@ -5,13 +5,18 @@ public class Position {
         public double x;
         public double y;
     
+        // constructor that initalizes the x and y variables 
         public Position(double inputX, double inputY) {
             x = inputX;
             y = inputY;
         }
+        
+        // returns x variable
         public double getX() {
             return x;
         }
+        
+        // returns y variable
         public double getY() {
             return y;
         }
