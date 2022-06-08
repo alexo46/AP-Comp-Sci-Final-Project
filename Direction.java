ca48e8542d2ac31ec9f5c6a94110c9473af852d5
Alex Oliva <98245982+alexo46@users.noreply.github.com>
2022-06-08 10:59:04 -0500

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -5,18 +5,23 @@ public class Direction {
     public double x;
     public double y;
 
+    // constructor that initalizes the variables
     public Direction(double inputX, double inputY) {
         x = inputX;
         y = inputY;
     }
+    
+    //changes the x and y variables to the variables passed
     public void set(double inputX, double inputY) {
         x = inputX;
         y = inputY;
     }
-
+    
+    // returns the x variable
     public double getX() {
         return x;
     }
+    // returns the y variable
     public double getY() {
         return y;
     }
